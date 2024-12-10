089b5f9bf6b1eddd700e3400aae4a2a06f48014b
Promise Falodun <promisefalodun@Promises-MacBook-Pro.local>
2024-12-10 10:37:22 +0100

    updaTING basic from with formik

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,11 +1,31 @@
 import { useFormik } from "formik";
 
 const BasicForm = () => {
-  const formik = useFormik();
+  const { values, handleChange, handleBlur } = useFormik(
+    {
+      initialValues: { email: "",
+        age:"",
+        password: "",
+        confirmPassword: "",
+       },
+    }
+  );
+  console.log(formik)
   return (
     <form autoComplete="off">
       <label htmlFor="email">Email</label>
-      <input id="email" type="email" placeholder="Enter your email" />
+      <input value={values.email} onChange={handleChange} onBlur={handleBlur}
+      id="email" type="email" placeholder="Enter your email" />
+      <label htmlFor="age">Age</label>
+      <input value={values.age} onChange={handleChange} onBlur={handleBlur}
+      id="age" type="number" placeholder="Enter your age" />
+      <label htmlFor="password">Password</label>
+      <input value={values.password} onChange={handleChange} onBlur={handleBlur}
+      id="password" type="password" placeholder="Enter your password" />
+      <label htmlFor="ConfirmPassword">Confirm Password</label>
+      <input value={values.confirmPassword} onChange={handleChange} onBlur={handleBlur}
+      id="ConfirmPassword" type="password" placeholder="Confirm Password" />
+          <button type="submit">Submit</button>
     </form>
   );
 };
